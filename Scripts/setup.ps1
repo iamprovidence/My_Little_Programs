49af69cae0f358e33db041dedc5b97a186ec9621
Kizlo Taras <orleon.nilkford@gmail.com>
2022-01-15 21:19:59 +0200

    Update setup.ps1

--- Scripts/setup.ps1
+++ Scripts/setup.ps1
@@ -1,4 +1,5 @@
-﻿Set-ExecutionPolicy Bypass -Scope Process -Force; 
+# Run in Administrator mode
+Set-ExecutionPolicy Bypass -Scope Process -Force; 
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 $Packages = @(
