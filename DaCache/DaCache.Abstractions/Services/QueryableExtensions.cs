3bda1c0126ccc354d6697c4145e1c74cebe30a37
Kizlo Taras <orleon.nilkford@gmail.com>
2022-03-30 10:46:11 +0300

    Add files via upload

---
+++ DaCache/DaCache.Abstractions/Services/QueryableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DaCache.Abstractions.Models;
+using DaCache.Abstractions.Interfaces;
+
+namespace DaCache.Abstractions.Services
+{
+    public static class QueryableExtensions
+    {
+        public static Task<List<T>> ToCachedList<T>(this IQueryable<T> query, ICacheService cacheService, CacheEntryOptions options = null)
+        {
+            return cacheService.GetOrAddItem(query.ToQueryString(), cacheOptions =>
+            {
+                if (options is not null)
+                {
+                    cacheOptions.Type = options.Type;
+                    cacheOptions.AbsoluteExpirationRelativeToNow = options.AbsoluteExpirationRelativeToNow;
+                    cacheOptions.SlidingExpiration = options.SlidingExpiration;
+                }
+
+                return query.ToListAsync();
+            });
+        }
+    }
+}
