9fe7c706450680a2f4b2e62dde10c745502ab192
Kizlo Taras <orleon.nilkford@gmail.com>
2022-03-30 13:55:21 +0300

    Add files via upload

---
+++ FunWithTests/FunWithTests.Mocha/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FunWithTests.Mocha
+{
+    public interface IService
+    {
+        string Hello();
+        string World();
+    }
+
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceMock = new global::Mocha.MyMock<IService>();
+
+            serviceMock
+                .Mock(m => m.Hello(), "Hello")
+                .Mock(m => m.World(), "World");
+
+            var service = serviceMock.GetObject();
+
+            Console.WriteLine($"{service.Hello()} {service.World()}");
+        }
+    }
+}
