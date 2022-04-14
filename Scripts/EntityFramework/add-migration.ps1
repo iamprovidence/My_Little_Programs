7725ad44e755cbab831972c301b5f629ac79b601
Kizlo Taras <orleon.nilkford@gmail.com>
2022-04-14 11:48:13 +0300

    Update add-migration.ps1

--- Scripts/EntityFramework/add-migration.ps1
+++ Scripts/EntityFramework/add-migration.ps1
@@ -5,8 +5,8 @@ dotnet ef migrations add $migrationName `
 	--startup-project ./Project.csproj ` # project with IDesignTimeDbContextFactory, DI, etc
 	--project ./Project.csproj `
 	--context DbContext `
-	--output-dir outputFolder `
+	--output-dir $outputFolder 
 
 $fileName = (Get-Item -Path "$outputFolder\*_$migrationName.cs").Name
 
-dotnet format --include "outputFolder\$fileName" --fix-analyzers warn
+dotnet format --include "$outputFolder\$fileName" --fix-analyzers warn
