75099265f6ead8b8b34ec2d1e840d90514117075
Kizlo Taras <orleon.nilkford@gmail.com>
2025-09-14 13:34:32 +0300

    Update ArchitectureTests.cs

--- WebArchitecture/WebArchitecture.NetArchTest/ArchitectureTests.cs
+++ WebArchitecture/WebArchitecture.NetArchTest/ArchitectureTests.cs
@@ -39,5 +39,49 @@ namespace WebArchitecture.NetArchTest
 				Assert.True(result.IsSuccessful, string.Join("\n", result.FailingTypeNames ?? Enumerable.Empty<string>()));
 			}
 		}
+
+		/*
+	  	// data-driven test
+	    [Fact]
+	    public void Validate_Cross_Layer_References()
+	    {
+	        var wrongReferences = new List<(string From, string To)>
+	        {
+	            ("Domain", "Application"),
+	            ("Domain", "Infrastructure"),
+	            ("Domain", "Presentation"),
+	    
+	            ("Application", "Infrastructure"),
+	            ("Application", "Presentation"),
+	
+	            ("Infrastructure", "Presentation"),
+	        };
+	
+	        var location = Assembly.GetExecutingAssembly().Location;
+	        var assemblies = Directory
+	            .EnumerateFiles(Path.GetDirectoryName(location), "PREFIX*.dll")
+	            .Select(AssemblyLoadContext.Default.LoadFromAssemblyPath)
+	            .ToList();
+	
+	        foreach (var layer in wrongReferences)
+	        {
+	            foreach (var assembly in assemblies) 
+	            {
+	                var references = assembly
+	                  .GetReferencedAssemblies()
+	                  .Where(x => x.FullName.StartsWith("Prefix"));
+	
+	                foreach (var reference in references) 
+	                {
+	                    var isReferenceAllowed = 
+	                      assembly.FullName.Contains(layer.From) &&
+	                      reference.FullName.Contains(layer.To)      
+	
+	                    Assert.False(isReferenceAllowed, $"Layer [{layer.From}] should not reference [{layer.To}]");
+	                }
+	            }
+	        }
+	    }
+  		*/
 	}
 }
